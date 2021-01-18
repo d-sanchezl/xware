940adf9bb982dcda8be748ee4390ca07c94671e5
d-sanchezl <16809335+d-sanchezl@users.noreply.github.com>
2021-01-18 11:56:51 -0500

    add om2m code

---
+++ clients/om2m_mqtt_gateway.py
@@ -0,0 +1,314 @@
+# MQTT to OM2M gateway device client with simulated aquisition
+
+# Import necessary packages
+import paho.mqtt.client as mqtt # MQTT
+import os # To create and manage directories
+import time # To time sending intervals
+
+# Import xware libraries
+from xware_lib_functions import *
+from xware_lib_om2m import *
+
+# =================================
+# PARAMETERS
+
+# Sampling Frequency
+F = 20000
+# Time length of a single sampling [s]
+t = 2
+# Time interval between samplings [s]
+T = 10
+# Voltage to value scaling
+valueConversion = 1/0.00989
+
+# Context tags
+deviceTag = 'accel'
+sensorTag = 'x_accel'
+
+# MQTT topic name
+deviceName = 'device0'
+# MQTT address, change to target IP address
+brokerAddress = "localhost"
+
+# OM2M names
+serverCSE = 'in-cse'
+serverName = 'in-name'
+containerName = 'sampling'
+eventsContName = 'events'
+authOM2M = 'admin:admin' # user:password
+
+# Simulated acquisition folder
+samplesLocation = "/home/pi/Documents/amest_txt_divided"
+
+# Time between message receipt verification
+waitTime = 0.1
+# Time between message sent retries
+retryWaitTime = 1
+# Maximum time before message sending stops and code exits
+maxWaitTime = 6
+
+# =================================
+# FUNCTIONS AND CALLBACKS
+
+def sendAndWaitMQTT(client,topic,payload,waitTime,retryWaitTime,maxWaitTime):
+    global newMessage
+    message = ''
+    newMessage = None
+    client.publish(topic, payload)
+    retryWaitTimer = time.time()
+    maxWaitTimer = time.time()
+    while not(newMessage):
+        if time.time() - maxWaitTimer > maxWaitTime:
+            print('Error: could not connect to OM2M')
+            exit()
+        if time.time() - retryWaitTimer > retryWaitTime:
+            client.publish(topic, payload)
+            retryWaitTimer = time.time()
+        time.sleep(waitTime)
+    newMessage = None
+
+def onConnect(client, userdata, flags, rc):
+    # userdata must hold the respective topic
+    client.subscribe(userdata)
+
+def onMessage(client, userdata, msg):
+    global newMessage
+    global message
+    newMessage = 1
+    message = str(msg.payload, 'utf-8')
+
+# Function: read next TXT line
+def getValueFromTxt():
+    global fileList
+    global currentFileIndex
+    global currentLineIndex
+    global lineList
+    # Read new file if necessary
+    if not(lineList):
+        file = open(fileList[currentFileIndex], 'r')
+        contents = file.read()
+        lineList = contents.splitlines()
+        file.close()
+    # Read the corresponding line
+    currentLine = lineList[currentLineIndex]
+    # Cycle to next valid line/file
+    currentLineIndex += 1
+    if currentLineIndex >= len(lineList):
+        currentFileIndex += 1
+        currentLineIndex = 0
+        lineList = []
+        if currentFileIndex >= len(fileList):
+            currentFileIndex = 0
+    # Extract value from full string
+    tabPos = currentLine.find('\t')
+    if tabPos == -1:
+        tabPos = currentLine.find(',')
+    valueStr = currentLine[tabPos+1:]
+    return valueStr
+
+
+# ======================
+# SET UP FLAGS AND VARIABLES
+# # globalStartSuccess = 0
+# # startSuccess = 0
+newMessage = None
+message = ''
+messageIndex = 0
+
+# Simulated Acquisition
+fileList = [(samplesLocation+"/"+i) for i in os.listdir(samplesLocation)]
+currentFileIndex = 0
+currentLineIndex = 0
+lineList = []
+
+
+# ======================
+# SET UP 'SIMULATED' DATA AQUISITION
+
+# Get names of all txt files in the folder
+fileNames = os.listdir(samplesLocation)
+
+# Read all file names in selected folder
+fileLocs = [(samplesLocation+"/"+i) for i in fileNames]
+totalFiles = len(fileLocs)
+fileIndex = 0
+
+# Simulation parameters
+deltaTime = 1/F
+sampleWaitTime = deltaTime/10
+
+
+#====================
+# OM2M SETTINGS
+topicReq = '/oneM2M/req/'+deviceName+'/'+serverCSE+'/json'
+topicResp = '/oneM2M/resp/'+serverCSE+'/'+deviceName+'/json'
+to_app  = '/'+serverCSE+'/'+serverName
+to_cont = '/'+serverCSE+'/'+serverName+'/'+deviceName
+to_data  = '/'+serverCSE+'/'+serverName+'/'+deviceName+'/'+containerName
+to_events  = '/'+serverCSE+'/'+serverName+'/'+deviceName+'/'+eventsContName
+
+
+#====================
+# WAIT FOR OPERATOR INPUT
+print('Selected sampling frequency is ' + str(F) + ' Hz')
+print('Selected sampling time is ' + str(t) + ' s')
+print('Selected sampling period is ' + str(T) + ' s')
+input('Press Return to begin...')
+print('')
+
+
+#====================
+# MQTT CONNECT TO OM2M
+client = mqtt.Client(deviceName, userdata = topicResp)
+client.on_message = onMessage
+client.on_connect = onConnect
+client.connect(brokerAddress)
+client.loop_start()
+
+
+# ===========
+# Check if OM2M application exists via MQTT
+payload = searchApplicationsPayload(authOM2M,to_app,'123456')
+# Send message and wait for response
+sendAndWaitMQTT(client,topicReq,payload,waitTime,retryWaitTime,maxWaitTime)
+# Check if this device is in the list
+if json.loads(message)['m2m:rsp']['m2m:pc']['m2m:uril']:
+    apps = lastUrlItem(json.loads(message)['m2m:rsp']['m2m:pc']['m2m:uril']['m2m:uril'])
+else: apps = []
+# Otherwise, create app
+if not(deviceName in apps):
+    payload = createApplicationPayload(authOM2M,to_app,'123456',F,t,T,valueConversion,deviceTag,sensorTag,deviceName)
+    sendAndWaitMQTT(client,topicReq,payload,waitTime,retryWaitTime,maxWaitTime)
+
+
+# ===========
+# Check if OM2M container exists via MQTT
+payload = searchContainersPayload(authOM2M,to_cont,'123456')
+# Send message and wait for response
+sendAndWaitMQTT(client,topicReq,payload,waitTime,retryWaitTime,maxWaitTime)
+# Check if this container is in the list
+if json.loads(message)['m2m:rsp']['m2m:pc']['m2m:uril']:
+    containers = lastUrlItem(json.loads(message)['m2m:rsp']['m2m:pc']['m2m:uril']['m2m:uril'])
+else: containers = []
+# Otherwise, create container
+if not(containerName in containers):
+    payload = createContainerPayload(authOM2M,to_cont,'123456',containerName)
+    sendAndWaitMQTT(client,topicReq,payload,waitTime,retryWaitTime,maxWaitTime)
+
+
+# ===========
+# Check if OM2M container (events) exists via MQTT
+# Otherwise, create container
+if not(eventsContName in containers):
+    payload = createContainerPayload(authOM2M,to_cont,'123456',eventsContName)
+    sendAndWaitMQTT(client,topicReq,payload,waitTime,retryWaitTime,maxWaitTime)
+
+
+#====================
+# OPEN EMPTY BUFFER
+deviceBuffer = '' # Store as str
+currentBufferSize = 0
+totalSamples = 0
+
+
+# ======================
+# Set clock for first period
+startTimePeriodT = time.time()
+currentTimePeriodT = startTimePeriodT
+nextTimePeriodT = currentTimePeriodT + T
+
+while 1:
+
+    print('Begin cycle!')
+
+    # ======================
+    # Start a new sampling cycle (t)
+    # Send START as MQTT+OM2M message, and wait for response
+    messageIndex += 1
+    payload = createMessagePayload(authOM2M,to_events,'123456','START\n'+deviceName+'\n'+str(messageIndex))
+    sendAndWaitMQTT(client,topicReq,payload,waitTime,retryWaitTime,maxWaitTime)
+
+    # ======================
+    # WAIT FOR  RESPONSE
+    ready = 0
+    while not(ready):
+        time.sleep(waitTime)
+
+        # Search for all messages
+        payload = searchMessagesPayload(authOM2M,to_events,'123456')
+        sendAndWaitMQTT(client,topicReq,payload,waitTime,retryWaitTime,maxWaitTime)
+        obj = json.loads(message)
+
+        # If there are messages
+        if obj['m2m:rsp']['m2m:pc']:
+            if obj['m2m:rsp']['m2m:pc']['m2m:uril']:
+                messageList = lastUrlItem(obj['m2m:rsp']['m2m:pc']['m2m:uril']['m2m:uril'])
+
+                # Read each message
+                for messageName in messageList:
+                    payload = readMessagePayload(authOM2M,to_events+'/'+messageName,'123456')
+                    sendAndWaitMQTT(client,topicReq,payload,waitTime,retryWaitTime,maxWaitTime)
+                    obj = json.loads(message)
+                    try:
+                        messageText = obj["m2m:rsp"]["m2m:pc"]["m2m:cin"]["con"][1:-1]
+                    except:
+                        messageText = '     '
+
+                    # Check if this is the message we need
+                    if messageText[:5] == 'TIMER':
+
+                        # Indicate readiness and delete received message
+                        ready = 1
+                        payload = deleteMessagePayload(authOM2M,to_events+'/'+messageName,'123456')
+                        sendAndWaitMQTT(client,topicReq,payload,waitTime,retryWaitTime,maxWaitTime)
+
+
+    # ======================
+    # Set clock for first sample
+    startTime = time.monotonic()
+    currentTime = startTime
+    nextTime = currentTime + deltaTime
+
+    # ======================
+    # 1/F loop: request sensor data
+
+    samplesInSampling = int(t*F)
+    while currentBufferSize < samplesInSampling:
+
+        # ======================
+        # Request and get sensor value
+        value = getValueFromTxt()
+
+        # Append value to device buffer
+        deviceBuffer += (str(value)+'\n')
+
+        # Update read samples
+        currentBufferSize += 1
+
+        # Stop the code until enough time has passed
+        while time.monotonic() < nextTime:
+            time.sleep(sampleWaitTime)
+
+        # Update current and next time
+        currentTime = nextTime
+        nextTime += deltaTime
+
+    # Debug
+    print('Done reading data! Sending buffer...')
+
+    # Send device buffer as MQTT+OM2M message
+    payload = createMessagePayload(authOM2M,to_data,'123456',deviceBuffer)
+    client.publish(topicReq, payload)
+
+    # Clear buffer
+    deviceBuffer = ''
+    currentBufferSize = 0
+
+    # Wait for next cycle
+    print('Done sending data! Waiting for next period...')
+    while time.time() < nextTimePeriodT:
+        time.sleep(sampleWaitTime)
+
+    # Update clock time for next sampling
+    currentTimePeriodT = nextTimePeriodT
+    nextTimePeriodT += T
---
+++ xrepo_integration/om2m_xrepo_builder.py
@@ -0,0 +1,204 @@
+# OM2M to XRepo
+
+# Import necessary packages
+import paho.mqtt.client as mqtt # MQTT
+import os # To create and manage directories
+import time # To time sending intervals
+
+# Import xware libraries
+from xware_lib_functions import *
+from xware_lib_om2m import *
+from xware_lib_xrepo import *
+
+# =================================
+# PARAMETERS
+
+# Simulated acquisition folder
+csvLocation = '/home/pi/Documents/gateway_buffer'
+
+# Log files location
+logLocation = '/home/pi/Documents/python'
+
+# OM2M parameters
+serverCSE = 'in-cse'
+serverName = 'in-name'
+containerName = 'sampling'
+eventsContName = 'events'
+ipOM2M = '127.0.0.1:8080'
+authOM2M = 'admin:admin'
+
+# Wait time between cycles (can be 0)
+waitTime = 0.05
+
+# CSV storage parameters
+timePrecision = 6
+valuePrecision = 5
+
+# XRepo parameters (changelog: added)
+sendToXRepo = 1 # set to 0 to disable XRepo uploading
+samplingId = "5f79e479e750ea746ad86fc9"
+usernameXRepo = 'user'
+passwordXRepo = 'user'
+
+
+# ======================
+# SET UP FLAGS AND VARIABLES
+
+# Variables
+startTimers = {}
+csvNumber = 0
+
+#====================
+# Check for directory existance and start LOG
+if not(os.path.isdir(csvLocation)):
+    os.mkdir(csvLocation)
+
+if not(os.path.isdir(logLocation)):
+    os.mkdir(logLocation)
+fullLogLoc = logLocation + '/' + 'log.txt'
+fullTimerLoc = logLocation + '/' + 'timer.txt'
+printAndLog('deviceName\tFlight\tCSV\tXRepo ',fullTimerLoc) #(changelog: XRepo)
+
+printAndLog('gateway is active',fullLogLoc)
+
+#====================
+# BEGIN CYCLING
+
+# Start buffers
+valueBuffer = []
+
+while 1:
+
+    # Get URL list of devices
+    applicationListUrl = listApplicationsREST(authOM2M,ipOM2M,serverCSE,serverName)
+    # Get device names from url's
+    devicesList = lastUrlItem(applicationListUrl)
+    # Number of devices
+    numOfDevices = len(devicesList)
+
+    # Create dictionary entry for devices if necessary
+    for deviceName in devicesList:
+        if not(deviceName in startTimers):
+            startTimers[deviceName] = {'currentIndex':0}
+
+    # Cycle through devices in list
+    for deviceName in devicesList:
+
+        #====================
+        # CHECK FOR RECEIVED EVENTS
+
+        # Get messages list
+        messageListUrl = listMessagesREST(authOM2M,ipOM2M,serverCSE,serverName,deviceName,eventsContName)
+        messageList = lastUrlItem(messageListUrl)
+
+        # Check if there are messages
+        for messageName in messageList:
+            # Process each message
+            messageText = getMessageREST(authOM2M,ipOM2M,serverCSE,serverName,deviceName,eventsContName,messageName)
+            if messageText[:5] == 'START':
+                # Delete message from OM2M
+                deleteMessageREST(authOM2M,ipOM2M,serverCSE,serverName,deviceName,eventsContName,messageName)
+                # Extract message contents
+                oldMessage = messageText.splitlines()
+                index = int(oldMessage[2])
+                # Store in dictio
+                startTimers[deviceName][index] = time.time()
+                # Talkback to device
+                newMessage = 'TIMERBEGIN\n'+deviceName+'\n'+str(index)
+                createMessageREST(authOM2M,ipOM2M,serverCSE,serverName,deviceName,eventsContName,newMessage)
+
+        #====================
+        # CHECK FOR RECEIVED VALUES
+
+        # Get messages list
+        messageListUrl = listMessagesREST(authOM2M,ipOM2M,serverCSE,serverName,deviceName,containerName)
+        messageList = lastUrlItem(messageListUrl)
+
+        # Check if there are pending messages
+        if messageList:
+
+            # Grab the first message in the list!
+            messageName = messageList[0]
+
+            # Download message contents from OM2M
+            valueBuffer = getMessageREST(authOM2M,ipOM2M,serverCSE,serverName,deviceName,containerName,messageName).splitlines()
+
+            # Read metadata of this device
+            labels = readApplicationLabels(authOM2M,ipOM2M,serverCSE,serverName,deviceName)
+            F = float(labels['Frequency[Hz]'])
+            valueConversion = float(labels['ValueConversion'])
+            deviceTag = labels['Device']
+            sensorTag = labels['Sensor']
+
+            # ===== Get start time =====
+            # Update current index for this device
+            startTimers[deviceName]['currentIndex'] += 1
+            currentIndex = startTimers[deviceName]['currentIndex']
+            startTime = startTimers[deviceName][currentIndex]
+
+            # Flight time
+            timerFlight = time.time() - startTime
+
+            # Create new CSV
+            fileName = 'sample_' + str(csvNumber) + '_' + deviceName + '.csv'
+            file = open(csvLocation + '/' + fileName, 'w')
+            file.write('ID,,,\n')
+
+            # Time delta
+            deltaTime = 1/F
+            currentTime = 0
+
+            # Create each CSV line
+            for valueStrRaw in valueBuffer:
+                timeUnix = preciseUnixTime(startTime,0,currentTime,timePrecision,string=1)
+                valueStr = float(valueStrRaw)*valueConversion
+                csvLine = csvFormatLine(timeUnix,deviceTag,sensorTag,valueStr,valuePrecision)
+                file.write(csvLine)
+                currentTime += deltaTime
+
+            # End CSV
+            file.close()
+            csvNumber += 1
+            printAndLog(deviceName + ' CSV file created',fullLogLoc)
+
+            # CSVTime
+            timerCSV = time.time() - startTime
+
+            # Upload to XRepo and delete from local storage (changelog: added)
+            if sendToXRepo:
+
+                # XREPO AUTH
+                token = xRepoAuth(usernameXRepo, passwordXRepo)
+                if not(token):
+                    printAndLog('XRepo authentication failed, terminating gateway',fullLogLoc)
+                    exit()
+                printAndLog(deviceName + ' XR authentication successful',fullLogLoc)
+
+                # XREPO SEND
+                code = xRepoSend(samplingId, fileName, csvLocation, token)
+                if not(code):
+                    printAndLog('XRepo upload failed, terminating gateway',fullLogLoc)
+                    exit()
+                else:
+                    os.remove(csvLocation+"/"+fileName)
+                printAndLog(deviceName + ' XR file uploaded',fullLogLoc)
+
+                # Append 2:XRTime
+                timerXR = time.time() - startTime
+
+            else:
+                timerXR = 0
+
+            # Print timers (changelog: added xrepo timer)
+            timerString = deviceName + '\t' + str(timerFlight) + \
+                          '\t' + str(timerCSV) + '\t' + str(timerXR)
+            printAndLog(timerString,fullTimerLoc)
+
+            # CLEAR gateway local buffer
+            valueBuffer = []
+
+            # CLEAR gateway OM2M buffer
+            deleteMessageREST(authOM2M,ipOM2M,serverCSE,serverName,deviceName,containerName,messageName)
+
+    # Let the program breathe!
+    time.sleep(waitTime)
